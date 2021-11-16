fde1694ca97b4c0ca5a2ee79982a483d858fd6d6
Andy Kaminski <akaminsk@ford.com>
2021-11-15 19:22:38 -0500

    cleanup and begining of maze run

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
 import random
 
-
 gamma = 0.9
 epsilon = 0.1
 total_visits_table = {}
 
 q_value_table = {}
 
-valid_starting_positions = ("B2","B3","B4","B5",
-                            "C3","C4","C6",
-                            "D3","D6",
-                            "E2","E3","E4","E5","E6",
+valid_starting_positions = ("B2", "B3", "B4", "B5",
+                            "C3", "C4", "C6",
+                            "D3", "D6",
+                            "E2", "E3", "E4", "E5", "E6",
                             )
-intial_rewards_table = {
-  "A1":-50,"A2":-50,"A3":-50,"A4":-50,"A5":-50,"A6":-50,"A7":-50,
-  "B1":-50,"B2":0,"B3":0,"B4":0,"B5":0,"B6":"WALL","B7":-50,
-  "C1":-50,"C2":"WALL","C3":0,"C4":0,"C5":"WALL","C6":0,"C7":-50,
-  "D1":-50,"D2":"WALL","D3":0,"D4":100,"D5":"WALL","D6":0,"D7":-50,
-  "E1":-50,"E2":0,"E3":0,"E4":0,"E5":0,"E6":0,"E7":-50,
-  "F1":-50,"F2":-50,"F3":-50,"F4":-50,"F5":-50,"F6":-50,"F7":-50,
+
+initial_rewards_table = {
+    "A1": -50, "A2": -50, "A3": -50, "A4": -50, "A5": -50, "A6": -50, "A7": -50,
+    "B1": -50, "B2": 0, "B3": 0, "B4": 0, "B5": 0, "B6": "WALL", "B7": -50,
+    "C1": -50, "C2": "WALL", "C3": 0, "C4": 0, "C5": "WALL", "C6": 0, "C7": -50,
+    "D1": -50, "D2": "WALL", "D3": 0, "D4": 100, "D5": "WALL", "D6": 0, "D7": -50,
+    "E1": -50, "E2": 0, "E3": 0, "E4": 0, "E5": 0, "E6": 0, "E7": -50,
+    "F1": -50, "F2": -50, "F3": -50, "F4": -50, "F5": -50, "F6": -50, "F7": -50,
 }
 
 reward_for_move = {
-    "NORTH":-3,
-    "SOUTH":-1,
-    "EAST":-2,
-    "WEST":-2
+    "NORTH": -3,
+    "SOUTH": -1,
+    "EAST": -2,
+    "WEST": -2
 }
 
 directions = ("NORTH",
@@ -37,30 +37,35 @@ directions = ("NORTH",
 def starting_position():
     return random.choice(valid_starting_positions)
 
-  
+
 def best_direction_to_move(position):
     best_direction = ""
-    #Use max q for best direction, unless hit by randomness introduced by epsilon
-    if ( random.random() <= epsilon) :
+    # Use max q for best direction, unless hit by randomness introduced by epsilon
+    if random.random() <= epsilon:
         best_direction = random.choice(directions)
-        print("Random direction triggered! for Position: ", position, 
-        "Now heading ", best_direction)
+        print("Random direction triggered! for Position: ", position,
+              "Now heading ", best_direction)
         return best_direction
 
     max_q_value = get_max_q_value(position)
 
-    list_of_directions_for_position = list(q_value_table.get(position).keys())
-    list_of_q_values_for_position = list(q_value_table.get(position).values())
-    index_of_max_q_value = list_of_q_values_for_position.index(max_q_value)
-
-    best_direction = list_of_directions_for_position[index_of_max_q_value]
+    # Translate the numeric value of max q position to an actual direction (N,S,E,W)
+    # Example q_values array from q_value_table.get(position):
+    # {"NORTH": 2, "SOUTH": 50, "EAST": 5, "WEST": 2}
+    directions_at_position_list = list(q_value_table.get(position).keys())
+    q_values_at_position_list = list(q_value_table.get(position).values())
+    index_of_max_q_value = q_values_at_position_list.index(max_q_value)
+    # Now that we have the index of the max_q value, it's the same as the index
+    # of the list of direction. Return the direction as a string.
+    best_direction = directions_at_position_list[index_of_max_q_value]
 
     return best_direction
 
+
 def get_max_q_value(position):
     all_directions_at_position = q_value_table.get(position)
-    
-    #TODO itterate through directions
+
+    # TODO iterate through directions
     q_values_all_directions = [
         all_directions_at_position.get(directions[0]),
         all_directions_at_position.get(directions[1]),
@@ -68,6 +73,7 @@ def get_max_q_value(position):
         all_directions_at_position.get(directions[3])
     ]
     return max(q_values_all_directions)
+
 
 def calculate_q_value(current_position, current_direction, future_position, future_direction):
     current_state_q = q_value_table.get(current_position).get(current_direction)
@@ -79,9 +85,17 @@ def calculate_q_value(current_position, current_direction, future_position, futu
     max_future_q_value = get_max_q_value(future_position)
 
     gamma_probability = current_state_q + number_of_tries_ratio * \
-         ( current_action_reward + gamma * \
-         max_future_q_value - current_state_q )
+                        (current_action_reward + gamma *
+                         max_future_q_value - current_state_q)
     return gamma_probability
 
+
+def adjust_position_for_drift():
+    return ""
+
+
 if __name__ == "__main__":
-    print("Hello")
+    starting_position = starting_position()
+    next_best_position = best_direction_to_move(starting_position)
+    next_actual_position = adjust_position_for_drift()
+
--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 import unittest
 import main
 
-class MainTests(unittest.TestCase):
 
+class MainTests(unittest.TestCase):
 
     def test_get_max_q_value(self):
         position_in_maze = "B2"
-        main.q_value_table = {"B2" : 
-            {"NORTH":5,"SOUTH":1,"EAST":1,"WEST":1}
-        }
+        main.q_value_table = {"B2":
+                                  {"NORTH": 5, "SOUTH": 1, "EAST": 1, "WEST": 1}
+                              }
         expected_max_q_value = 5
         actual_max_q_value = main.get_max_q_value(position_in_maze)
 
@@ -17,13 +17,13 @@ class MainTests(unittest.TestCase):
     def test_next_direction_is_max_value_zero_epsilon(self):
         main.epsilon = 0
         expected_direction = "NORTH"
-        main.q_value_table = {"B2" : 
-            {"NORTH":5,"SOUTH":1,"EAST":1,"WEST":1}
-        }
+        main.q_value_table = {"B2":
+                                  {"NORTH": 5, "SOUTH": 1, "EAST": 1, "WEST": 1}
+                              }
         calculated_direction = main.best_direction_to_move("B2")
-         
+
         self.assertEqual(expected_direction, calculated_direction)
-        
+
     def test_ramdom_room_is_valid_room(self):
         message = "Initial random entry into 'maze' is a valid room"
         list_of_valid_rooms_string = "".join(main.valid_starting_positions)
@@ -36,15 +36,16 @@ class MainTests(unittest.TestCase):
         current_direction = "EAST"
         future_position = "B3"
         future_direction = "SOUTH"
-        main.total_visits_table["B2"] = {"NORTH":0,"SOUTH":0,"EAST":9,"WEST":0} 
-        main.q_value_table["B2"] = {"NORTH":0,"SOUTH":0,"EAST":5,"WEST":0} 
-        main.q_value_table["B3"] = {"NORTH":2,"SOUTH":50,"EAST":5,"WEST":2} 
-        
-        explected_q_value = 8.8
-        actual_q_value = main.calculate_q_value(current_position,
-        current_direction, future_position, future_direction)
+        main.total_visits_table["B2"] = {"NORTH": 0, "SOUTH": 0, "EAST": 9, "WEST": 0}
+        main.q_value_table["B2"] = {"NORTH": 0, "SOUTH": 0, "EAST": 5, "WEST": 0}
+        main.q_value_table["B3"] = {"NORTH": 2, "SOUTH": 50, "EAST": 5, "WEST": 2}
 
-        self.assertEqual(explected_q_value, actual_q_value)
+        expected_q_value = 8.8
+        actual_q_value = main.calculate_q_value(current_position,
+                                                current_direction, future_position, future_direction)
+
+        self.assertEqual(expected_q_value, actual_q_value)
+
 
 if __name__ == '__main__':
     unittest.main()
